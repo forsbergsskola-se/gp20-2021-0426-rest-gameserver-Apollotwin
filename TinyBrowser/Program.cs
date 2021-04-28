dc38a681f6bb2edcbe34aa2289558d83b840f039
Simon Magnusson <simon.magnusson@student.forsbergsskola.se>
2021-04-28 10:17:00 +0200

    Creates TCP Client that get response from acme.com

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TinyBrowser
 {
@@ -6,7 +8,20 @@ namespace TinyBrowser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tcpClient = new TcpClient("acme.com", 80);
+            if (tcpClient.Connected)
+            {
+                Console.WriteLine("Connected");
+                var stream = tcpClient.GetStream();
+                stream.Write(Encoding.UTF8.GetBytes("GET \r\n"));
+                var buffer = new byte[1024];
+                var readStream = stream.Read(buffer, 0, buffer.Length);
+                var stringBuilder = new StringBuilder().AppendFormat("{0}", Encoding.ASCII.GetString(buffer, 0, readStream));
+                Console.WriteLine(stringBuilder);
+                stream.Close();
+                tcpClient.Close();
+            }
+            
         }
     }
 }
