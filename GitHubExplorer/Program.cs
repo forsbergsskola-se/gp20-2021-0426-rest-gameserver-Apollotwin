8ccb959b866a3fb4c659d1b46c7201c1b3c84050
Simon Magnusson <simon.magnusson@student.forsbergsskola.se>
2021-05-17 13:12:40 +0200

    Testing stuff in GithubExplorer

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace GitHubExplorer
 {
@@ -8,9 +10,23 @@ namespace GitHubExplorer
     {
         static void Main(string[] args)
         {
-            string username = Console.ReadLine();
+            //Console.WriteLine("Enter a user you want to inspect: ");
+            string username = "Apollotwin";
+            string myToken = "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";
+            string header = $"Authorization: token {myToken}";
             var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add($"https://api.github.com/users/{username}", username);
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.github.com/users/{username}");
+            httpClient.DefaultRequestHeaders.Add("Authorization",$"token {myToken}" );
+            httpClient.Send(request);
+            Console.WriteLine(request);
+            var response = new HttpResponseMessage();
+            var stream = new StreamReader(response.Content.ReadAsStream());
+            Console.WriteLine(response);
+            Console.WriteLine(response.Headers);
+            Console.WriteLine(stream.ReadToEnd());
+            var streamToJson = JsonSerializer.Deserialize<string>(stream.ReadToEnd());
+            Console.WriteLine(streamToJson);
+            //Console.WriteLine(request.Content);
         }
     }
 }
